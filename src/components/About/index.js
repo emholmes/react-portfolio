85b8e2e5fea0b3f41817ef1e88bf9aaf7ddcc7a8
Erin Holmes <e.holmes09@yahoo.com>
2021-09-22 17:06:36 -0500

    style about me page for desktop

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,15 +2,17 @@ import headshot from "../../images/erin-h-square.jpeg"
 
 const About = () => {
   return (
-    <section id="about-me" className="about-me">  
+    <section>  
       <h3>About Me</h3>
-      <div>
+      <div className="about-me">
+        <div>
           <p>Hi, I'm Erin.</p>
           <p> 
               I am a detail oriented front-end developer with experience in responsive web design and development, web accessibility, digital activation, project management and design experimentation.
           </p>
+        </div>
+        <img src={headshot} alt="headshot of Erin Holmes" />
       </div>
-      <img src={headshot} alt="headshot of Erin Holmes" />
     </section>
   )
 }
