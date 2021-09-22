b698200918b030c515d68090d1c50a8c6760b5a3
Erin Holmes <e.holmes09@yahoo.com>
2021-09-22 11:18:50 -0500

    set up react app, create component folders, initial set up for header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <div class="h1-block">
+        <h1>Erin Holmes</h1>
+            <h2>Web Developer</h2>
+      </div>
+    </header>
+    
+  )
+}
+
+export default Header;
