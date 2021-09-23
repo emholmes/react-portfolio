b8e0382307ca11ffa7aff44f04aa4ade070e564f
Erin Holmes <e.holmes09@yahoo.com>
2021-09-23 14:16:19 -0500

    move projects info to json file

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,30 +1,6 @@
-import { useState } from "react";
+import projects from "../../projects.json";
 
 const Portfolio = () => {
-  const [projects] = useState([
-    {
-      id: "project-one", name: "Parktime 2.0", description: "MVC &#183; Sequelize &#183; Node.js &#183; RESTful API", pageUrl: "https://parktime-2.herokuapp.com/", githubUrl: ""
-    },
-    {
-      id: "project-two", name: "Tech Blog", description: "MVC &#183; Express &#183; Sequelize", pageUrl: "https://tech-blog-mvc-eh.herokuapp.com/", githubUrl: ""
-    },
-    {
-      id: "project-three", name: "Note Taker", description: "Express.js", pageUrl: "https://notetaker-eh.herokuapp.com/", githubUrl: ""
-    },
-    {
-      id: "project-four", name: "Workday Scheduler", description: "Bootstrap &#183; jQuery &#183; Third-party API", pageUrl: "https://emholmes.github.io/eholmes-work-day-scheduler-api-challenge/", githubUrl: ""
-    },
-    {
-      id: "project-five", name: "Weather Dashboard", description: "Javascript &#183; Server-side API", pageUrl: "https://emholmes.github.io/api-weather-dashboard/", githubUrl: ""
-    },
-    {
-      id: "project-six", name: "JS Fundamentals Quiz", description: "Javascript", pageUrl: "https://emholmes.github.io/eholmes-js-quiz-api-challenge/", githubUrl: ""
-    },
-    {
-      id: "project-seven", name: "Password Generator", description: "Javascript", pageUrl: "https://emholmes.github.io/eholmes-password-generator-javascript-challenge", githubUrl: ""
-    }
-  ]);
-  
   return (
     <section id="my-work" className="my-work">
       <h3>Portfolio</h3>
