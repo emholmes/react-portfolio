cd9049bb46677401025ffa35aadc8bce9c9415cd
Erin Holmes <e.holmes09@yahoo.com>
2021-09-24 16:20:57 -0500

    add validate email helper, add state to contact form, add error handling to contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,14 +1,57 @@
+import { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
+
 const ContactForm = () => {
+  const [ formState, setFormState ] = useState({ name: "", email: "", message: "" });
+  const { name, email, message } = formState;
+  const [ errorMessage, setErrorMessage ] = useState("");
+
+  function validateContactForm(event) {
+    if (event.target.name === "email") {
+      const isValid = validateEmail(event.target.value);
+      if (!isValid) {
+        setErrorMessage("The entered is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!event.target.value.length) {
+        setErrorMessage(`${event.target.name} required`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({...formState, [event.target.name]: event.target.value });
+    }
+  }
+
+  const submitContactForm = (event) => {
+    event.preventDefault();
+    console.log(formState);
+  }
+
   return (
     <section className="contact">
       <h3>Contact</h3>
-      <form className="contact-form">
-        <label htmlFor="name">Name:</label>
-        <input type="text" name="name" />
-        <label htmlFor="email">Email address:</label>
-        <input type="email" name="email" />
-        <label htmlFor="message">Message:</label>
-        <textarea name="message" rows="5" />
+      <form className="contact-form" onSubmit={submitContactForm}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" defaultValue={name} onBlur={validateContactForm} name="name" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" defaultValue={email} onBlur={validateContactForm} name="email" required />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" defaultValue={message} onBlur={validateContactForm} rows="5" required />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
         <button type="submit">Submit</button>
       </form>
     </section>
