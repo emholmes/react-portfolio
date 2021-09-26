1523977390722e80453bfb62563f57db1c681f91
Erin Holmes <e.holmes09@yahoo.com>
2021-09-26 10:18:43 -0500

    fix spacing

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,10 +1,10 @@
 const Project = ({ project }) => {
   return (
-      <div className="project-description"> 
-        <a href={project.pageUrl} className="project-title" target="_blank" rel="noreferrer">{project.name}</a>
-        <a href={project.githubUrl} target="_blank" rel="noreferrer"><span className="icon icon-github"></span></a>
-        <p>{project.description}</p>
-      </div>
+    <div className="project-description"> 
+      <a href={project.pageUrl} className="project-title" target="_blank" rel="noreferrer">{project.name}</a>
+      <a href={project.githubUrl} target="_blank" rel="noreferrer"><span className="icon icon-github"></span></a>
+      <p>{project.description}</p>
+    </div>
   )
 }
 
