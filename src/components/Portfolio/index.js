65e5270148efa936140bade7b8126055a5b5c094
Erin Holmes <e.holmes09@yahoo.com>
2021-09-26 10:18:16 -0500

    set up Portfolio to map through projects, make project stand alone component

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,10 +1,17 @@
 import Project from "../Project";
+import projects from "../../projects.json";
 
 const Portfolio = () => {
   return (
     <section id="my-work" className="my-work">
       <h3>Portfolio</h3>
-      <Project />
+      <ul className="project-list">
+        {projects.map((project) => (
+          <li className={project.id} key={project.id}>
+            <Project project={project} />
+          </li>
+        ))}
+      </ul>
     </section>
   )
 }
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,18 +1,10 @@
-import projects from "../../projects.json";
-
-const Project = () => {
+const Project = ({ project }) => {
   return (
-    <ul className="project-list">
-      {projects.map((project) => (
-        <li className={project.id} key={project.id}>
-            <div className="project-description"> 
-              <a href={project.pageUrl} className="project-title" target="_blank" rel="noreferrer">{project.name}</a>
-              <a href={project.githubUrl} target="_blank" rel="noreferrer"><span className="icon icon-github"></span></a>
-              <p>{project.description}</p>
-            </div>
-        </li>
-      ))}
-    </ul>
+      <div className="project-description"> 
+        <a href={project.pageUrl} className="project-title" target="_blank" rel="noreferrer">{project.name}</a>
+        <a href={project.githubUrl} target="_blank" rel="noreferrer"><span className="icon icon-github"></span></a>
+        <p>{project.description}</p>
+      </div>
   )
 }
 
