d27c7e63d1b44edb475092f6d233a187f8bcedbb
Erin Holmes <e.holmes09@yahoo.com>
2021-09-26 17:09:51 -0500

    add hackerrank icon, style footer icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,22 @@
-import ghIcon from "../../assets/icons/icon-github-min.png";
-import liIcon from "../../assets/icons/icon-linkedin-min.png";
-
 const Footer = () => {
   return (
     <footer>
       <section id="contact-me" className="contact-me">
         <ul className="contact-me-links">
           <li>
-              <a href="https://www.linkedin.com/in/erin-holmes-26861945/" target="_blank" rel="noreferrer"><img src={liIcon} alt="LinkedIn icon"/></a>
+              <a href="https://www.linkedin.com/in/erin-holmes-26861945/" target="_blank" rel="noreferrer">
+                <span className="icon icon-linkedin"></span>
+              </a>
           </li>
           <li>
-              <a href="https://github.com/emholmes" target="_blank" rel="noreferrer"><img src={ghIcon} alt="GitHub icon" /></a>
+              <a href="https://github.com/emholmes" target="_blank" rel="noreferrer">
+                <span className="icon icon-github"></span>
+              </a>
+          </li>
+          <li>
+              <a href="https://github.com/emholmes" target="_blank" rel="noreferrer">
+                <span className="icon icon-hackerrank"></span>
+              </a>
           </li>
         </ul>
         <p>Made by Erin Holmes.</p>
