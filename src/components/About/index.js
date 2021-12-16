0385c8ab7feb7d324c0e100fa7742a3229886ea2
Erin Holmes <e.holmes09@yahoo.com>
2021-12-16 11:24:06 -0600

    update about me copy

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,7 +12,7 @@ const About = () => {
           </p>
           {/* <hr /> */}
           <p>
-            I'm currently enrolled in a Full-Stack Web Development Bootcamp through The University of Texas at Austin, graduating mid-October 2021.  
+            I'm completed a Full-Stack Web Development Bootcamp through The University of Texas at Austin, October 2021.  
           </p>
         </div>
         <img src={headshot} alt="headshot of Erin Holmes" />
